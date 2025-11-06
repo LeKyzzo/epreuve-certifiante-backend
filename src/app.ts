4af106087147f9507ddf85d387d9153128146249
Matéo Journiac <journiacmateo@gmail.com>
2025-11-06 14:36:23 +0100

    ajout d'une config swagger et expo de l'ui sur /docs

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import cors from 'cors';
 import express, { type Request, type Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import routes from './routes';
+import swaggerSpec from './config/swagger';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/health', (_req: Request, res: Response) => {
   res.json({ statut: 'ok' });
